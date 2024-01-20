568325c0ace836329ca57b0b36b98e5b9c4bede8
JomarRomeroBalmores <78462090+TheColdPrince@users.noreply.github.com>
2024-01-20 12:22:58 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,55 +5,52 @@ import sys
 import fildem
 
 with open('README.md', 'r') as fh:
-	long_description = fh.read()
+    long_description = fh.read()
 
 
+def fildem_startup():
+    return input("Would you like to run fildem on startup? [Y/N]: ").lower()
 
-def fildemStartup():
-	return input("Would you like to run fildem on startup? [Y/N]: ")
 
-def __Main__():
-	if (sys.argv[1] == "install"):
-		response = fildemStartup().lower()
-		if (response == "y" or response == "yes"):
-			os.system("mv ./fildemstartup/* ~/.config/autostart/")
-		elif (response == "n" or response == "no"):
-			print("")
-		else:
-			#Ask the question again
-			__Main__()
+def main():
+    if len(sys.argv) > 1 and sys.argv[1] == "install":
+        response = fildem_startup()
+        if response in ("y", "yes"):
+            os.system("mv ./fildemstartup/* ~/.config/autostart/")
+        elif response in ("n", "no"):
+            print("")
+        else:
+            # Ask the question again
+            main()
 
-__Main__()
+
+main()
 
 setuptools.setup(
-	name='fildem',
-	version=fildem.__version__,
-	author='Gonzalo',
-	author_email='gonzaarcr@gmail.com',
-	description='Fildem Global Menu for Gnome Desktop',
-	long_description=long_description,
-	long_description_content_type='text/markdown',
-	url='https://github.com/gonzaarcr/Fildem',
-	packages=setuptools.find_packages(),
-	data_files=[
-		('share/applications', ['fildem-hud.desktop'])
-	],
-	install_requires=[
-		'PyGObject>=3.30.0'
-	],
-	classifiers=[
-		'Programming Language :: Python :: 3',
-		'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-		'Operating System :: POSIX :: Linux'
-	],
-	project_urls={
-		'Bug Reports': 'https://github.com/gonzaarcr/Fildem/issues',
-		'Source': 'https://github.com/gonzaarcr/Fildem',
-	},
-	entry_points={
-		'console_scripts': [
-			'fildem = fildem.run:main',
-			'fildem-hud = fildem.inithud:main'
-		]
-	}
+    name='fildem',
+    version=fildem.__version__,
+    author='Gonzalo',
+    author_email='gonzaarcr@gmail.com',
+    description='Fildem Global Menu for Gnome Desktop',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/gonzaarcr/Fildem',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'PyGObject>=3.30.0'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: POSIX :: Linux'
+    ],
+    project_urls={
+        'Bug Reports': 'https://github.com/gonzaarcr/Fildem/issues',
+        'Source': 'https://github.com/gonzaarcr/Fildem',
+    },
+    entry_points={
+        'console_scripts': [
+            'fildem = fildem.run:main',
+        ]
+    }
 )
